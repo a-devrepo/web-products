757f3dc751a9815ca60a4dedb51ef4ee1db77539
alison <adevpr11@gmail.com>
2026-06-03 13:14:46 -0300

    Commit inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-products');
+
+  httpClient = inject(HttpClient)
+
+  nomeProduto: string = '';
+
+  url: string = 'http://localhost:8081/api/v1/produtos/listar?nome=';
+
+  pesquisarProdutos(){
+    this.httpClient.get(`${this.url}${this.nomeProduto}`)
+    .subscribe(data => {
+      console.log(data);
+    });
+  }
 }
